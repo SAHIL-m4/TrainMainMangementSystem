984c29bdff3654f2f735af564124da40da69ec0a
SAHIL-m4 <mohammedsahilmac4@gmail.com>
2026-03-30 10:51:26 +0530

    UC4- use case 4 is over ---.

--- src/TrainMainMangementSystem.java
+++ src/TrainMainMangementSystem.java
@@ -1,37 +1,23 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 public class TrainMainMangementSystem {
     public static void main(String[] args) {
         System.out.println("==========================================");
-        System.out.println("   === Train Consist Management App ===");
-        System.out.println("==========================================");
-        System.out.println();
-        List<String> trainConsist = new ArrayList<>();
-        System.out.println("Train initialized successfully...");
-        System.out.println("Initial Bogie Count : " + trainConsist.size());
-        System.out.println("Current Train Consist : " + trainConsist);
-        System.out.println();
-        System.out.println("==========================================");
-        System.out.println(" UC3 - Track Unique Bogie IDs ");
-        System.out.println("==========================================");
-        System.out.println();
-        Set<String> bogies = new HashSet<>();
-        bogies.add("BG101");
-        bogies.add("BG102");
-        bogies.add("BG103");
-        bogies.add("BG104");
-        bogies.add("BG101");
-        bogies.add("BG102");
-        System.out.println("Bogie IDs After Insertion:");
-        System.out.println(bogies);
-        System.out.println();
-        System.out.println("Note:");
-        System.out.println("Duplicates are automatically ignored by HashSet.");
-        System.out.println();
-        System.out.println("UC3 uniqueness validation completed...");
-        System.out.println();
-        System.out.println("System ready for operations...");
+        System.out.println(" UC4 - Track Bogie Capacities ");
+        System.out.println("==========================================\n");
+        Map<String, Integer> bogieCapacities = new HashMap<>();
+        bogieCapacities.put("Sleeper", 72);
+        bogieCapacities.put("AC Chair", 60);
+        bogieCapacities.put("First Class", 24);
+        System.out.println("Bogie Capacities (Type -> Seats):");
+        System.out.println(bogieCapacities + "\n");
+        String checkBogie = "AC Chair";
+        System.out.println("Fetching capacity for '" + checkBogie + "':");
+        System.out.println("Capacity: " + bogieCapacities.get(checkBogie) + " seats\n");
+        System.out.println("Iterating through all Bogie Capacities:");
+        for (Map.Entry<String, Integer> entry : bogieCapacities.entrySet()) {
+            System.out.println("Bogie Type: " + entry.getKey() + " | Capacity: " + entry.getValue());
+        }
+        System.out.println("\nUC4 map operations completed successfully...");
     }
 }
