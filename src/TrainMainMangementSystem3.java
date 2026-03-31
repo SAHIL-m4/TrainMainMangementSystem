14b86e013aa85938931cfb4de0f7dd134acf517b
SAHIL-m4 <mohammedsahilmac4@gmail.com>
2026-03-31 13:01:28 +0530

    UC14-use case 14 over!! .

---
+++ src/TrainMainMangementSystem3.java
@@ -0,0 +1,35 @@
+public class TrainMainMangementSystem3 { 
+    static class InvalidCapacityException extends Exception {
+        public InvalidCapacityException(String message) {
+            super(message);
+        }
+    }
+    static class PassengerBogie {
+        String type;
+        int capacity;
+        PassengerBogie(String type, int capacity) throws InvalidCapacityException {
+            if (capacity <= 0) {
+                throw new InvalidCapacityException("Capacity must be greater than zero");
+            }
+            this.type = type;
+            this.capacity = capacity;
+            System.out.println("Created Bogie: " + type + " -> " + capacity);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("================================================");
+        System.out.println(" UC14 - Handle Invalid Bogie Capacity ");
+        System.out.println("================================================\n");
+        try {
+            new PassengerBogie("Sleeper", 72);
+        } catch (InvalidCapacityException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        try {
+            new PassengerBogie("AC Chair", 0);
+        } catch (InvalidCapacityException e) {
+            System.out.println("Error: " + e.getMessage());
+        } 
+        System.out.println("\nUC14 exception handling completed...");
+    }
+}
