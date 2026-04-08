eb027d9e1a98158ace8a88156f9cbdf8f14fea9b
jaslyn-j <jaslynjacob026@gmail.com>
2026-04-08 09:31:06 +0530

    UC17thhh

--- src/TrainMainMangementSystem.java
+++ src/TrainMainMangementSystem.java
@@ -1,28 +1,18 @@
+import java.util.Arrays;
 public class TrainMainMangementSystem {
     public static void main(String[] args) {
-        System.out.println("===========================================");
-        System.out.println(" UC16 - Manual Sorting using Bubble Sort ");
-        System.out.println("===========================================\n");
-        int[] capacities = {72, 56, 24, 70, 60};
-        System.out.println("Original Capacities:");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println("==============================================");
+        System.out.println(" UC17 - Sort Bogie Names Using Arrays.sort() ");
+        System.out.println("==============================================");
         System.out.println();
-        for (int i = 0; i < capacities.length - 1; i++) {
-            for (int j = 0; j < capacities.length - 1 - i; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-        System.out.println("\nSorted Capacities (Ascending):");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
+        System.out.println("Original Bogie Names:");
+        System.out.println(Arrays.toString(bogieNames));
         System.out.println();
-        System.out.println("\nUC16 sorting completed...");
+        Arrays.sort(bogieNames);
+        System.out.println("Sorted Bogie Names (Alphabetical):");
+        System.out.println(Arrays.toString(bogieNames));
+        System.out.println();
+        System.out.println("UC17 sorting completed...");
     }
 }
